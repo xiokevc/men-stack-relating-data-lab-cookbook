915aa2acfa13a80d8a238526d3d6328ce2d11d79
xiokevc <xiokevc@gmail.com>
2025-07-27 13:42:44 -0400

    add int middleware

---
+++ controllers/food.js
@@ -0,0 +1,10 @@
+// controllers/foods.js
+
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+// router logic will go here - will be built later on in the lab
+
+module.exports = router;
